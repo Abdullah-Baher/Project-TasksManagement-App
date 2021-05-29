115636ba2d6dd7402799d09f160839c2879ce6ec
Abdullah-Baher <62217391+Abdullah-Baher@users.noreply.github.com>
2021-05-29 06:18:27 +0200

    Text-Error added

--- src/components/DisplayTaskModal.js
+++ src/components/DisplayTaskModal.js
@@ -1,8 +1,15 @@
 import { React, useState } from 'react'
 import DatePicker from 'react-datepicker'
 import { Container, Form, ListGroup } from 'react-bootstrap'
+import Styled from 'styled-components'
 import "react-datepicker/dist/react-datepicker.css";
 import "../ModalStyle.css"
+
+const FieldError = Styled.p`
+    font-size: 18px;
+    color: red;
+    margin-top: 5px;
+`
 
 const usernames = ['Abdullah Baher', 'Salah Mostafa', 'Shehab Khalid', 'Ahmed Salama', 'Mohamed Hatem', 'Ali Adel'];
 
@@ -23,16 +30,19 @@ const DisplayTaskModal = ({ task }) => {
                 <Form.Label style={{fontSize: '20px'}}>Task Title</Form.Label>
                 <Form.Control type="text" size="lg" id='task-title' defaultValue={task.title} required />
                 <Form.Text className="text-muted">Task title should not be more than three words</Form.Text>
+                <FieldError id="task-title-error"></FieldError>
             </Form.Group>
 
             <Form.Group controlId="BasicTaskDescription">
                 <Form.Label style={{fontSize: '20px'}}>Task Description</Form.Label>
-                <Form.Control type="text" size="lg" id='task-description' defaultValue={task.description} required />    
+                <Form.Control type="text" size="lg" id='task-description' defaultValue={task.description} required />
+                <FieldError id="task-description-error"></FieldError>
             </Form.Group>
 
             <Form.Group controlId="BasicTaskDeadline">
                 <Form.Label style={{fontSize: '20px'}}>Task Deadline</Form.Label>
                 <DatePicker wrapperClassName='datePicker' id='deadline' selected={selectedDate} onChange={date => setSelectedDate(date)} required />
+                <FieldError id="deadline-error"></FieldError>
             </Form.Group>
 
             <Form.Group controlId="BasicTaskMembers">
@@ -47,6 +57,7 @@ const DisplayTaskModal = ({ task }) => {
                         </ListGroup.Item>)
                     }
                 </ListGroup>
+                <FieldError id="task-members-error"></FieldError>
             </Form.Group>
         </Container>
     )
