2c22ae73a3120f931b4a298aa5d4d9396c01ff9a
Abdullah-Baher <62217391+Abdullah-Baher@users.noreply.github.com>
2021-05-29 06:14:39 +0200

    Text-Errors added

--- src/components/CreateTaskModal.js
+++ src/components/CreateTaskModal.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import { Container, Form, ListGroup } from 'react-bootstrap'
+import Styled from 'styled-components'
 import "react-datepicker/dist/react-datepicker.css";
 import "../ModalStyle.css"
+
+const FieldError = Styled.p`
+    font-size: 18px;
+    color: red;
+    margin-top: 5px;
+`
 
 const usernames = ['Abdullah Baher', 'Salah Mostafa', 'Shehab Khalid', 'Ahmed Salama', 'Mohamed Hatem', 'Ali Adel'];
 
@@ -20,20 +27,24 @@ const CreateTaskModal = () => {
 
     return (
         <Container fluid>
+           
             <Form.Group controlId="BasicTaskTitle">
                 <Form.Label style={{fontSize: '20px'}}>Task Title</Form.Label>
-                <Form.Control type="text" size="lg" id='task-title' placeholder='Enter Task Title' required />
+                <Form.Control type="text" size="lg" id='task-title' placeholder='Enter Task Title' required="true" />
                 <Form.Text className="text-muted">Task title should not be more than three words</Form.Text>
+                <FieldError id="task-title-error"></FieldError>
             </Form.Group>
 
             <Form.Group controlId="BasicTaskDescription">
                 <Form.Label style={{fontSize: '20px'}}>Task Description</Form.Label>
-                <Form.Control type="text" size="lg" id='task-description' placeholder='Enter Task Description' required />    
+                <Form.Control type="text" size="lg" id='task-description' placeholder='Enter Task Description' required />
+                <FieldError id="task-description-error"></FieldError>    
             </Form.Group>
 
             <Form.Group controlId="BasicTaskDeadline">
                 <Form.Label style={{fontSize: '20px'}}>Task Deadline</Form.Label>
                 <DatePicker wrapperClassName='datePicker' id='deadline' selected={selectedDate} onChange={date => setSelectedDate(date)} required />
+                <FieldError id="deadline-error"></FieldError>
             </Form.Group>
             
             <Form.Group controlId="BasicTaskMembers">
@@ -47,7 +58,9 @@ const CreateTaskModal = () => {
                         </ListGroup.Item>)
                     }
                 </ListGroup>
+                <FieldError id="task-members-error"></FieldError>
             </Form.Group>
+            
         </Container>
            
     )
